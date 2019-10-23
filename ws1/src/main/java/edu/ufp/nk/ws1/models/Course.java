06315c90802ca13ee5e0b2a411a5d997d7b64aa8
Pedro Alves <55798885+pedroalvesk@users.noreply.github.com>
2019-10-23 18:50:29 +0100

    Prototype v0.2

---
+++ ws1/src/main/java/edu/ufp/nk/ws1/models/Course.java
@@ -0,0 +1,45 @@
+package edu.ufp.nk.ws1.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Course extends BaseModel {
+
+	// Variables
+	private String name;
+
+	@OneToOne
+	private Degree degree;
+
+	// Constructor
+	public Course(String name){
+		this.name = name;
+	}
+
+
+	// Gets & Sets
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public Degree getDegree(){
+		return degree;
+	}
+	public void setDegree(Degree degree) {
+		this.degree = degree;
+	}
+
+
+}
--- ws1/src/main/java/edu/ufp/nk/ws1/repositories/DegreeRepo.java
+++ ws1/src/main/java/edu/ufp/nk/ws1/repositories/DegreeRepo.java
@@ -4,9 +4,8 @@ import edu.ufp.nk.ws1.models.Degree;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface DegreeRepo extends CrudRepository <Degree, Long>{
+public interface DegreeRepo extends CrudRepository <Degree, Long> {
 	Degree findByName(String name);
 	Degree findById(long id);
 }
