043174b4f493bf1eca0a4d06a44928a64d45ae3c
Pedro Alves <55798885+pedroalvesk@users.noreply.github.com>
2019-12-11 14:58:34 +0000

    Update Degree.java

--- ws1/src/main/java/edu/ufp/nk/ws1/models/Degree.java
+++ ws1/src/main/java/edu/ufp/nk/ws1/models/Degree.java
@@ -4,10 +4,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 
 @Data
