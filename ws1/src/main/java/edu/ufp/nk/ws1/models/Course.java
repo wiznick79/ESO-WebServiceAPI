0a5cebfa1fbdea2dead8b9074c36ead8ca6fb07b
Pedro Alves <55798885+pedroalvesk@users.noreply.github.com>
2019-12-11 14:58:06 +0000

    Update Course.java

--- ws1/src/main/java/edu/ufp/nk/ws1/models/Course.java
+++ ws1/src/main/java/edu/ufp/nk/ws1/models/Course.java
@@ -3,7 +3,6 @@ package edu.ufp.nk.ws1.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 
