4bfdb445efe77f1bca370f09c4c9ccfc9087a73e
wiznick <wiznick79@yahoo.gr>
2020-01-11 19:16:42 +0000

    added student/explainer checks on POST /appointment

--- ws1/src/main/java/edu/ufp/nk/ws1/models/Explainer.java
+++ ws1/src/main/java/edu/ufp/nk/ws1/models/Explainer.java
@@ -3,7 +3,10 @@ package edu.ufp.nk.ws1.models;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import java.util.HashSet;
 import java.util.Set;
 
