238df753c46d3cec0886dc28b9d38aca924f5e1f
pedroalvesk <pedroalvesk@protonmail.com>
2020-01-12 03:14:58 +0000

    Added Search v3 - Fix Day

--- ws1/src/main/java/edu/ufp/nk/ws1/repositories/AvailabilityRepo.java
+++ ws1/src/main/java/edu/ufp/nk/ws1/repositories/AvailabilityRepo.java
@@ -4,12 +4,13 @@ import edu.ufp.nk.ws1.models.Availability;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Optional;
 
 @Repository
 public interface AvailabilityRepo extends CrudRepository<Availability, Long> {
     Optional<Availability> findById (long id);
-    Optional<Availability> findByDayOfWeekAndStart (int day, LocalTime start);
+    Optional<Availability> findByDayAndStart (LocalDate day, LocalTime start);
 
 }
