d20e94384621864447ed492f397bd8404ed7c388
dka98 <tos.industries@gmail.com>
2020-01-17 23:58:37 +0000

    university CONTROLLER

--- ws2/src/main/java/edu/ufp/nk/ws2/services/UniversityService.java
+++ ws2/src/main/java/edu/ufp/nk/ws2/services/UniversityService.java
@@ -1,7 +1,35 @@
 package edu.ufp.nk.ws2.services;
 
+import edu.ufp.nk.ws2.models.University;
+import edu.ufp.nk.ws2.repositories.UniversityRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UniversityService {
+
+    @Autowired
+    private UniversityRepo universityRepo;
+
+    public Iterable<University> getAllUniversities(){
+        return universityRepo.findAll();
+    }
+
+    public Optional<University> getUniversityById(Long id){
+        if (universityRepo.existsById(id)){
+            return universityRepo.findById(id);
+        }
+        return Optional.empty();
+    }
+
+    public Optional<University> createUniversity(University university){
+        Optional<University> optionalUniversity = this.universityRepo.findByName(university.getName());
+        if (optionalUniversity.isPresent()){
+            return Optional.empty();
+        }
+        University createUniversity = this.universityRepo.save(university);
+        return Optional.of(createUniversity);
+    }
 }
