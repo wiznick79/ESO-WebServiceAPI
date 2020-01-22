435084b9dad88f618dbcb4b2650f2bd5823606e3
dka98 <tos.industries@gmail.com>
2020-01-22 16:44:37 +0000

    92%

--- ws1/src/test/java/edu/ufp/nk/ws1/BootstrapTest.java
+++ ws1/src/test/java/edu/ufp/nk/ws1/BootstrapTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(classes = Bootstrap.class)
 @VisibleForTesting
@@ -29,7 +30,7 @@ public class BootstrapTest {
     @Test
     @VisibleForTesting
     void bootstrap(){
-
-
+        Bootstrap bootstrap = new Bootstrap(degreeRepo,courseRepo,studentRepo,collegeRepo);
+        assertNotNull(bootstrap);
     }
 }
--- ws1/src/test/java/edu/ufp/nk/ws1/services/filters/FilterExplainerServiceTest.java
+++ ws1/src/test/java/edu/ufp/nk/ws1/services/filters/FilterExplainerServiceTest.java
@@ -1,17 +1,29 @@
 package edu.ufp.nk.ws1.services.filters;
 
 import edu.ufp.nk.ws1.models.Availability;
+import edu.ufp.nk.ws1.models.Degree;
 import edu.ufp.nk.ws1.models.Explainer;
 import edu.ufp.nk.ws1.models.Language;
+import edu.ufp.nk.ws1.services.filters.explainer.FilterExplainerObject;
+import edu.ufp.nk.ws1.services.filters.explainer.FilterExplainerService;
 import org.assertj.core.util.VisibleForTesting;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class FilterExplainerServiceTest {
+    @MockBean
+    private FilterExplainerService filterExplainerService;
     @Test
     @VisibleForTesting
     void filter() {
@@ -21,6 +33,7 @@ public class FilterExplainerServiceTest {
         Set<Explainer> explainers = new HashSet<>();
         Set<Language> languages = new HashSet<>();
         Set<Availability> availabilities = new HashSet<>();
+        Degree degree = new Degree("Engenharia Informatica");
         Explainer explainer = new Explainer("Nikos Perris");
         explainers.add(explainer);
         Language language = new Language("Greek");
@@ -29,9 +42,26 @@ public class FilterExplainerServiceTest {
         availabilities.add(availability);
         explainer.setAvailabilities(availabilities);
         explainer.setLanguages(languages);
+        explainer.setDegree(degree);
 
+        Map<String, String> map = new HashMap<>();
+        map.put("name","Nikos Perris");
+        map.put("day", "1998-01-12");
+        map.put("start","10:00");
+        map.put("end","12:00");
+        map.put("degree", "Engenharia Informatica");
+        map.put("language", "Greek");
+        FilterExplainerObject filterExplainerObject= new FilterExplainerObject(map);
+        filterExplainerObject.getExplainerName();
+        filterExplainerObject.getDay();
+        filterExplainerObject.getStart();
+        filterExplainerObject.getEnd();
+        filterExplainerObject.getDegree();
+        filterExplainerObject.getLanguage();
+        filterExplainerObject.getDay();
+        assertNotNull( filterExplainerObject.getExplainerName());
+        //filterExplainerService.filter(explainers,filterExplainerObject);
 
-        //FilterExplainerObject filterExplainerObject= new FilterExplainerObject();
 
 
     }
